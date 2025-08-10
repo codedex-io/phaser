d2bdf9b37077e09642d8d4185959d222641d987d
Julien Kris <hi.jules.kris@gmail.com>
2025-08-10 17:06:10 -0400

    update ready set config

--- 1-your-first-quest/03-ready-set-config.js
+++ 1-your-first-quest/03-ready-set-config.js
@@ -2,31 +2,10 @@
 // Codédex
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: "#002fa7",
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#8ACE00"
 };
 
 const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("background", "https://i.imgur.com/Joul9GG.png");
-}
-
-function create() {
-  const width = this.sys.game.config.width;
-  const height = this.sys.game.config.height;
-
-  this.add.image(width / 2, height / 2, "background");
-}
-
-function update() {
-  // We'll add interactivity later
-}
-
