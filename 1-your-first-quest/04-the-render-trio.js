75473252ad7c017b024f9c67ce111d67f4da557e
Julien Kris <hi.jules.kris@gmail.com>
2025-08-10 17:16:02 -0400

    Update 04-the-render-trio.js

--- 1-your-first-quest/04-the-render-trio.js
+++ 1-your-first-quest/04-the-render-trio.js
@@ -1,6 +1,7 @@
 // The Render Trio ☘️
 // Codédex
 
+// Config object
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -13,19 +14,25 @@ const config = {
   }
 };
 
+// Create a new Phaser game instance
 const game = new Phaser.Game(config);
 
+// Load assets before the game starts
 function preload() {
   this.load.image("background", "https://i.imgur.com/Joul9GG.png");
 }
 
+// Place assets on the screen
 function create() {
+  // Get game canvas width and height from the config
   const width = this.sys.game.config.width;
   const height = this.sys.game.config.height;
 
+  // Add the background image centered on the canvas
   this.add.image(width / 2, height / 2, "background");
 }
 
+// Update game logic every frame (empty for now)
 function update() {
   // We'll add interactivity later
 }
