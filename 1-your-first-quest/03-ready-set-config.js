731a129a50e3f5abffdbd6f80d7747af4c4980ac
Julien Kris <hi.jules.kris@gmail.com>
2025-08-10 17:01:08 -0400

    Create 03-ready-set-config.js

---
+++ 1-your-first-quest/03-ready-set-config.js
@@ -0,0 +1,11 @@
+// Ready, Set, Config! 🏁
+// Codédex
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#8ACE00"
+};
+
+const game = new Phaser.Game(config);
