c6264a732d356833169c82f02d95682d4301d4fd
Julien Kris <hi.jules.kris@gmail.com>
2025-08-10 17:16:05 -0400

    Update 05-wasd.js

--- 1-your-first-quest/05-wasd.js
+++ 1-your-first-quest/05-wasd.js
@@ -1,27 +1,34 @@
 // WASD ↔️
 // Codédex
 
+// Declare global variables
 let player;
 let cursors;
 
-function preload() { 
+function preload() {
+  // Load the character image
   this.load.image("character", "https://i.imgur.com/TTQA8lp.gif");
 }
 
 function create() {
+  // Add the player to the center of the screen
   player = this.add.image(400, 300, "character");
+
+  // Enable arrow key input
   cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
   const speed = 3;
-  
+
+  // Left/right movement
   if (cursors.left.isDown) {
     player.x -= speed;
   } else if (cursors.right.isDown) {
     player.x += speed;
   }
 
+  // Up/down movement
   if (cursors.up.isDown) {
     player.y -= speed;
   } else if (cursors.down.isDown) {
@@ -29,6 +36,7 @@ function update() {
   }
 }
 
+// Phaser configuration
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -40,4 +48,5 @@ const config = {
   }
 };
 
+// Create the Phaser game instance
 const game = new Phaser.Game(config);
