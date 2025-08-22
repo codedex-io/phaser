da70e5e32fbd7fb5dd1f6e3c10061ae0fe5e2fcb
Julien Kris <hi.jules.kris@gmail.com>
2025-08-22 15:44:58 -0400

    Update 01-press-start.js

--- 1-your-first-quest/01-press-start.js
+++ 1-your-first-quest/01-press-start.js
@@ -1,9 +1,9 @@
 // Press Start 💻
 // Codédex
 
-console.log('https://dentrala.itch.io/elf-mail'); 
-console.log('You play as an elf who is also a mail delivery person'); 
-console.log('Use arrow keys to move left and right, space to jump, and down + space to drop'); 
-console.log('Collide with the envelope to collect it'); 
-console.log('Bring it to the correct window to deliver the mail'); 
+console.log("https://dentrala.itch.io/elf-mail"); 
+console.log("You play as an elf who is also a mail delivery person"); 
+console.log("Use arrow keys to move left and right, space to jump, and down + space to drop"); 
+console.log("Collide with the envelope to collect it"); 
+console.log("Bring it to the correct window to deliver the mail"); 
 
