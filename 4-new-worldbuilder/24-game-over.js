40484ca03608df6ecd42d5d4e264a838c1f88102
Julien Kris <hi.jules.kris@gmail.com>
2025-12-03 15:35:38 -0500

    Update 24-game-over.js

--- 4-new-worldbuilder/24-game-over.js
+++ 4-new-worldbuilder/24-game-over.js
@@ -1,56 +1,106 @@
 // Game Over 🛑
 // Codédex
 
-let player;
-let coins;
-let enemy;
-let platforms;
-let gameOver = false;
-let gameWon = false;
-let coinsCollected = 0;
-let totalCoins = 3;
-
-function preload() {
-  this.load.image("player", "https://i.imgur.com/NSsqbju.png");
-  this.load.image("platform", "https://i.imgur.com/uEJoszN.png");
-  this.load.image("coin", "https://i.imgur.com/mB3HMeU.png");
-  this.load.image("enemy", "https://i.imgur.com/ChV1G4V.png");
-}
-
-function create() {
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 580, "platform").setScale(2.5).refreshBody();
-
-  player = this.physics.add.sprite(150, 450, "player").setScale(2.5);
-  this.physics.add.collider(player, platforms);
-
-  enemy = this.physics.add.sprite(500, 450, "enemy").setScale(2.5);
-  this.physics.add.collider(enemy, platforms);
-  this.physics.add.collider(player, enemy, () => {
-    gameOver = true;
-  });
-
-  coins = this.physics.add.group();
-  const c1 = coins.create(200, 500, "coin");
-  const c2 = coins.create(400, 500, "coin");
-  const c3 = coins.create(600, 500, "coin");
-  coins.children.iterate(c => (c.body.allowGravity = false));
-
-  this.physics.add.overlap(player, coins, (player, coin) => {
-    coin.disableBody(true, true);
-    coinsCollected += 1;
-    if (coinsCollected === totalCoins) gameWon = true;
-  });
-}
-
-function update() {
-  if (player.y > 600) gameOver = true;
-}
-
-new Phaser.Game({
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: { default: "arcade", arcade: { gravity: { y: 700 }, debug: true } },
+  physics: { default: "arcade", arcade: { gravity: { y: 700 }, debug: true }},
   scene: { preload, create, update },
-});
+};
+
+let player;
+let cursors;
+let collectibles;
+let score = 0;
+let lives = 3;
+let scoreText;
+let livesText;
+let enemy;
+let gameOver = false;
+
+function preload() {
+  this.load.image("frog", "https://i.imgur.com/NSsqbju.png");
+
+  this.load.image("greenPlatform", "https://i.imgur.com/uEJoszN.png");
+  this.load.image("yellowPlatform", "https://i.imgur.com/uhAy5nF.png");
+  this.load.image("bluePlatform", "https://i.imgur.com/oSHt5fT.png");
+  this.load.image("pinkPlatform", "https://i.imgur.com/TUKptDb.png");
+
+  this.load.image("greenObstacle", "https://i.imgur.com/1Xm9qHQ.png");
+  this.load.image("yellowObstacle", "https://i.imgur.com/3UbYQNw.png");
+  this.load.image("blueObstacle", "https://i.imgur.com/yqdBMzJ.png");
+  this.load.image("pinkObstacle", "https://i.imgur.com/noxAg7A.png");
+
+  this.load.image("collectible1", "https://i.imgur.com/mB3HMeU.png");
+  this.load.image("collectible2", "https://i.imgur.com/tF52I4f.png");
+  this.load.image("collectible3", "https://i.imgur.com/MktA2DV.png");
+  this.load.image("collectible4", "https://i.imgur.com/I40gewR.png");
+
+  this.load.image("enemy", "https://i.imgur.com/sdFz5Xp.png");
+  this.load.image("bg", "https://i.imgur.com/Sjz9xBy.png");
+}
+
+function create() {
+  this.add.image(400, 300, "bg").setScale(1.3);
+
+  const platforms = this.physics.add.staticGroup();
+  const gp = platforms.create(150, 500, "greenPlatform").setScale(2.5).refreshBody();
+
+  player = this.physics.add.sprite(150, 460, "frog").setScale(2.5);
+
+  collectibles = this.physics.add.group();
+  const c1 = collectibles.create(gp.x, gp.y - 150, "collectible1").setScale(1.5);
+  c1.body.allowGravity = false;
+
+  enemy = this.physics.add.sprite(600, 450, "enemy").setScale(2);
+  enemy.setVelocityX(120);
+  enemy.setBounce(1, 0);
+  enemy.setCollideWorldBounds(true);
+
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(enemy, platforms);
+  this.physics.add.overlap(player, collectibles, collectItem, null, this);
+  this.physics.add.collider(player, enemy, hitEnemy, null, this);
+
+  scoreText = this.add.text(16, 16, "Score: 0", { fontSize: "32px", fill: "#fff" });
+  livesText = this.add.text(16, 50, "Lives: 3", { fontSize: "32px", fill: "#fff" });
+
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function collectItem(player, item) {
+  item.disableBody(true, true);
+  score += 10;
+  scoreText.setText("Score: " + score);
+}
+
+function hitEnemy() {
+  if (gameOver) return;
+
+  lives--;
+  livesText.setText("Lives: " + lives);
+
+  if (lives <= 0) {
+    gameOver = true;
+    player.setTint(0xff0000);
+    player.setVelocity(0);
+  } else {
+    player.setPosition(150, 460);
+  }
+}
+
+function update() {
+  if (gameOver) return;
+
+  if (cursors.left.isDown) player.setVelocityX(-200);
+  else if (cursors.right.isDown) player.setVelocityX(200);
+  else player.setVelocityX(0);
+
+  if ((cursors.up.isDown || cursors.space.isDown) && player.body.touching.down)
+    player.setVelocityY(-450);
+
+  if (player.y > config.height) hitEnemy();
+}
+
+new Phaser.Game(config);
